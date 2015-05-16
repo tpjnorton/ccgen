950914fd6eb4ca36210c9f01f10333444732d87c
Tom Norton <tpjnorton@hotmail.co.uk>
2015-05-16 23:49:25 +0100

    Reads files as lists of tokens and entire lines.

--- ccgen.cpp
+++ ccgen.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include "stylegen.hpp"
 
 using namespace std;
+
+class readFile
+{
+	public:
+		ifstream i;
+		vector<string> lineList;
+		vector<string> tokenList;
+		stringstream tokenbuf;
+		//stringstream linebuf;
+		string word,line;
+
+		readFile(char* s) : i((s)) {}
+
+		void get_contents()
+		{
+			while (i.peek() != EOF)
+			{
+				getline(i,line);
+				stringstream linebuf(line);
+				lineList.push_back(line);
+
+				while (linebuf.good())
+				{
+					getline(linebuf,word,' ');
+					tokenList.push_back(word);
+			    	// cout << word << endl;
+				}
+			}
+		}
+
+};
+
 
 void usage()
 {
@@ -13,6 +49,8 @@ void usage()
 
 int main(int argc, char* argv[])
 {
+	readFile file(argv[1]);
+	file.get_contents();
 	cout << "--------ccgen: A lightweight boilerplate C generator--------" << endl;
 	cout << "---------------------Version 0.0.1--------------------------" << endl;
 	if (argc < 3)
@@ -20,5 +58,12 @@ int main(int argc, char* argv[])
 		cout << "ERROR: No input Arguments Specified" << endl;
 		usage();
 	}
+	// unsigned i = 0;
+	// while (i<file.tokenList.size())
+	// {
+	// 	cout << file.tokenList[i] << endl;
+	// 	i++;
+	// }
+
 	return 0;
 }
