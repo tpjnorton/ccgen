c04429cdcf545440fcc71bcb6e612b45f56faa98
Tom Norton <tpjnorton@hotmail.co.uk>
2015-05-17 02:52:49 +0100

    V0.0.1.1 Can now read basic scripts and generate styled C code.

--- ccgen.cpp
+++ ccgen.cpp
@@ -6,11 +6,15 @@
 #include <fstream>
 #include <sstream>
 #include "styletype.hpp"
+#include <unistd.h>
 
 #define DEF 	0
 #define	KNR 	1
 
 using namespace std;
+
+int codingStyle = 0;
+stringstream outputbuffer;
 
 class readFile
 {
@@ -81,6 +85,163 @@ void usage()
 	exit(1);
 }
 
+bool parseTokens(readFile &file)
+{
+	bool understood = false;
+	outputbuffer << def_include;
+	while (file.hasMoreTokens())
+	{
+		string currToken = file.getNextToken();
+		understood = false;
+
+		if (currToken == "STYLE")
+		{
+			currToken = file.getNextToken();
+			
+			if (currToken == "DEF")
+			{
+				codingStyle = DEF;
+				understood = true;
+			}
+
+			else if (currToken == "KNR")
+			{
+				codingStyle = KNR;
+				understood = true;
+			}
+		}
+
+		else if (currToken == "IF")
+		{
+			currToken = file.getNextToken();
+			
+			if (currToken == "MULT")
+			{
+				if (codingStyle == KNR)
+				{
+					outputbuffer << if_knr;
+					understood = true;
+				}
+
+				else if (codingStyle == DEF)
+				{
+					outputbuffer << if_default;
+					understood = true;
+				}
+
+				understood = true;
+			}
+
+			else if (currToken == "ONE")
+			{
+				outputbuffer << if_one_line;
+				understood = true;
+			}
+		}
+
+		else if (currToken == "ELIF")
+		{
+			currToken = file.getNextToken();
+			
+			if (currToken == "MULT")
+			{
+				if (codingStyle == KNR)
+				{
+					outputbuffer << else_if_knr;
+					understood = true;
+				}
+
+				else if (codingStyle == DEF)
+				{
+					outputbuffer << else_if_default;
+					understood = true;
+				}
+
+				understood = true;
+			}
+
+			else if (currToken == "ONE")
+			{
+				outputbuffer << else_if_one_line;
+				understood = true;
+			}
+		}
+
+		else if (currToken == "ELSE")
+		{
+			currToken = file.getNextToken();
+			
+			if (currToken == "MULT")
+			{
+				if (codingStyle == KNR)
+				{
+					outputbuffer << else_knr;
+					understood = true;
+				}
+
+				else if (codingStyle == DEF)
+				{
+					outputbuffer << else_default;
+					understood = true;
+				}
+
+				understood = true;
+			}
+
+			else if (currToken == "ONE")
+			{
+				outputbuffer << else_one_line;
+				understood = true;
+			}
+		}
+
+		else if (currToken == "MAIN")
+		{
+			currToken = file.getNextToken();
+			
+			if (currToken == "ARG")
+			{
+				if (codingStyle == KNR)
+				{
+					outputbuffer << arg_main_knr;
+					understood = true;
+				}
+
+				else if (codingStyle == DEF)
+				{
+					outputbuffer << arg_main_default;
+					understood = true;
+				}
+
+				understood = true;
+			}
+
+			else if (currToken == "VOID")
+			{
+				if (codingStyle == KNR)
+				{
+					outputbuffer << void_main_knr;
+					understood = true;
+				}
+
+				else if (codingStyle == DEF)
+				{
+					outputbuffer << void_main_default;
+					understood = true;
+				}
+
+				understood = true;
+			}
+		}
+
+
+		if (!understood) return false;
+	}
+
+	if (understood) return true;
+	return false;
+}
+
 int main(int argc, char* argv[])
 {
 	cout << "--------ccgen: A lightweight boilerplate C generator--------" << endl;
@@ -91,11 +252,24 @@ int main(int argc, char* argv[])
 		usage();
 	}
 	readFile file(argv[1]);
+	cout << "Reading file..." << endl;
 	file.getContents();
-	while (file.hasMoreTokens())
-	{
-		cout << file.getNextToken() << endl;
-	}
+	usleep(500000);
+	cout << "File Read Succesful." << endl;
+	usleep(500000);
+	cout << "Parsing file..." << endl;
+	usleep(500000);
+	bool retval = parseTokens(file);
+	cout << "DONE" << endl;
+	cout << "parse = "<< boolalpha << retval << endl;
+	string currentStructure;
+	ofstream o(argv[2]);
 
-	return 0;
+
+
+	currentStructure = outputbuffer.str();
+	o << currentStructure;
+
+
+	return retval;
 }
