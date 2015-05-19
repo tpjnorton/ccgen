94364d87acd3fc54e4adafad5907e9b98da020b1
Tom Norton <tpjnorton@hotmail.co.uk>
2015-05-19 05:31:02 +0100

    Fixed .gitignore

---
+++ test/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void function0{
+
+    if(    ){
+
+    }
+
+    else if(    ){
+
+    }
+
+    if(    ) ;
+
+}
+
+char function1{
+
+    if(    ){
+
+    }
+
+    if(    ){
+
+    }
+
+    if(    ) ;
+
+}
+
+int main(void){
+    return 0;
+}
+
